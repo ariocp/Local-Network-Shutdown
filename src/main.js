1c0a04928d7ad968d00f6dca413a11344cb052f8
ariocp <ariocp@rambler.ru>
2023-11-10 04:32:34 +0300

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	deleted:    main.js
    	new file:   src/main.js
    	renamed:    get-hosts.js -> src/utils/get-hosts.js
    	renamed:    shutdown.js -> src/utils/shutdown.js

---
+++ src/main.js
@@ -0,0 +1,45 @@
+const { exec } = require("child_process");
+const { stdout } = require("process");
+
+let command = "arp -a";
+
+const executeCommand = async (command) => {
+    return new Promise((resolve, reject) => {
+        exec(command, (error, stdout) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(stdout);
+            }
+        });
+    });
+};
+
+const executeShutdownCommand = async (address) => {
+    return new Promise((resolve, reject) => {
+        command = `shutdown /s /m \\\\${address}`;
+
+        exec(command, (error, stdout) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(stdout);
+            }
+        });
+    });
+};
+
+const processAddresses = async (addresses) => {
+    addresses.forEach(async (address) => {
+        executeShutdownCommand(address);
+    });
+};
+
+executeCommand(command)
+    .then((stdout) => {
+        const addresses = stdout.split("\n");
+        processAddresses(addresses);
+    })
+    .catch((error) => {
+        console.error(error);
+    })
